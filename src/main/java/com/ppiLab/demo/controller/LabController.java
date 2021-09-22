a00fe4b32d1901f9d6f70a848a7d19ff2b6efe74
Nathan Augusto <nathanacds@algartelecom.com.br>
2021-09-22 18:38:38 -0300

    [BSI‐PI]Laboratorio04‐SpringBoot

---
+++ src/main/java/com/ppiLab/demo/controller/LabController.java
@@ -0,0 +1,39 @@
+package com.ppiLab.demo.controller;
+
+import com.ppiLab.demo.model.Item;
+import com.ppiLab.demo.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "/lab04")
+public class LabController {
+
+    @GetMapping(path = "/add")
+    public @ResponseBody String addUser(@RequestParam String name, @RequestParam String email) {
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        return n.toString();
+    }
+
+    @GetMapping(path = "/addItem")
+    public @ResponseBody String addItem(@RequestParam String name, @RequestParam String description, @RequestParam long price, @RequestParam int quantity){
+
+        Item item = new Item();
+        item.setName(name);
+        item.setDescription(description);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        return item.toString();
+
+
+    }
+
+
+
+}
---
+++ src/main/java/com/ppiLab/demo/model/Item.java
@@ -0,0 +1,65 @@
+package com.ppiLab.demo.model;
+
+public class Item {
+
+    private long id;
+
+    private int quantity;
+
+    private long price;
+
+    private String name;
+
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
